4531c8249de16289a859803885e522ae3803cf1b
Johel-code <jbsch16@gmail.com>
2025-03-10 14:17:26 -0400

    chore: servicio lote agregado

---
+++ servicio-lote/src/main/java/com/proyecto/servicio_lote/ServicioLoteApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.servicio_lote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioLoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioLoteApplication.class, args);
+	}
+
+}
