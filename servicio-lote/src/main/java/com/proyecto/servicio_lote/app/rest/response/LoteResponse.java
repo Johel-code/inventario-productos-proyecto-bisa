5e9ba0270bb25945dcaf1d283fb0f33ce21f558d
Johel-code <jbsch16@gmail.com>
2025-03-12 20:35:46 -0400

    fix: correccion de costo compra por aumento en entidad lote

--- servicio-lote/src/main/java/com/proyecto/servicio_lote/app/rest/response/LoteResponse.java
+++ servicio-lote/src/main/java/com/proyecto/servicio_lote/app/rest/response/LoteResponse.java
@@ -2,12 +2,14 @@ package com.proyecto.servicio_lote.app.rest.response;
 
 import com.proyecto.servicio_lote.domain.models.Proveedor;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record LoteResponse (
         Long id,
         Long productoId,
         Integer cantidad,
+        BigDecimal costoCompra,
         LocalDate fechaAquisicion,
         LocalDate fechaExpiracion,
         ProveedorResponse proveedor
